651cbfb587da3c820a4404984bbae45dfdf340b7
unknown <magale.thobejane@gmail.com>
2023-07-19 22:22:38 +0200

    llllease enter the commit message for your changes. Lines starting

--- average_age.sql
+++ average_age.sql
@@ -1,1 +1,9 @@
 /* List the average age (in years) of employees by month. */
+
+SELECT
+    DATEPART(MONTH, BirthDate) AS Month,
+    AVG(DATEDIFF(YEAR, BirthDate, GETDATE())) AS AverageAge
+FROM
+    employees
+GROUP BY
+    DATEPART(MONTH, BirthDate)
--- doh1986.sql
+++ doh1986.sql
@@ -1,1 +1,4 @@
 /* List employees who were hired in 1986. */
+
+for the fact it is true
+jfl] ;s;s; kdld;d.fgg
